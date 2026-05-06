89819ba0b93035727998827a8d3c6cb51910ef38
Yang Zou <yaz128@ucsd.edu>
2026-05-05 21:13:21 -0700

    break test for PR

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -1,5 +1,11 @@
 // sum.test.js
 
+import { sum } from '../code-to-unit-test/sum.js';
+
 test('adds 1 + 2 to equal 3', () => {
-  // TODO
+  expect(sum(1, 2)).toBe(4);
+});
+
+test('adds 2 + 3 to equal 5', () => {
+  expect(sum(2, 3)).toBe(5);
 });
