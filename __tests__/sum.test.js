31d4568851916e249f8be28a1597c721c2736db3
Yang Zou <yaz128@ucsd.edu>
2026-05-05 21:51:49 -0700

    fix test after screenshot

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -3,7 +3,7 @@
 import { sum } from '../code-to-unit-test/sum.js';
 
 test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(4);
+  expect(sum(1, 2)).toBe(3);
 });
 
 test('adds 2 + 3 to equal 5', () => {
